24b6f74d81541dcfa67e935fd2cb3abc61e32ef4
RohiniShejwal <rohiniwagh12@gmail.com>
2023-11-28 17:06:30 +0530

    m

---
+++ src/main/java/com/BikkadIT/QualifierAnnotation/AutowiringModeByNameApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.QualifierAnnotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutowiringModeByNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutowiringModeByNameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/QualifierAnnotation/AutowiringModeByNameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.QualifierAnnotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutowiringModeByNameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
